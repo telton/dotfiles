b73d633fb293c11c71e5b3faaa135a5346c786e3
Tyler Elton <tylerelton@smartsalesandlease.com>
2019-06-14 10:27:48 -0400

    Add install file for required dependencies.

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Install antigen.
+curl -L git.io/antigen > ~/antigen.zsh
+
+# Install vundle.
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install zsh and curl.
+sudo apt-get update && sudo apt-get install zsh curl -y
+
+# Install oh my zsh.
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Make a backup of the default .zshrc file.
+cp ~/.zshrc ~/.zshrc.back
+
+# Install new .zshrc
+cp .zshrc ~/.zshrc
+
+# Install zsh theme.
+cp classyTouch.zsh-theme ~/.oh-my-zsh/themes/
+
+# Fetch new source.
+echo 'Done!'
+source ~/.zshrc

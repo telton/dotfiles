11bfff37b7f81c7e57339485e85b138329c00b60
Tyler Elton <tylerelton@smartsalesandlease.com>
2019-06-14 10:32:21 -0400

    Typo.

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ sudo apt-get update && sudo apt-get install zsh curl -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Make a backup of the default .zshrc file.
-cp ~/.zshrc ~/.zshrc.back
+cp ~/.zshrc ~/.zshrc.bak
 
 # Install new .zshrc
 cp .zshrc ~/.zshrc

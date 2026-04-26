353ec8dfe1ec6afb4b18882f72a22acb4e811178
Tyler Elton <telton007@gmail.com>
2026-04-25 21:51:32 -0400

    fix(neovim): moar treesitter stuff

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
+  branch = 'main',
   build = ':TSUpdate',
   config = function()
     local ts = require 'nvim-treesitter'
